b4a9a5dda40d9cbd745ae62209bc6333ae09d061
Tclow97 <tianchun.low@gmail.com>
2018-10-23 10:19:26 +0800

    tc commit

---
+++ testExtension1/borderify.js
@@ -0,0 +1,17 @@
+document.body.style.border = "5px solid red";
+
+// page-script.js
+
+// add a new element to the DOM
+// var p = document.createElement("p");
+// p.textContent = "This paragraph was added by a page script.";
+// p.setAttribute("id", "page-script-para");
+// document.body.appendChild(p);
+
+// // define a new property on the window
+// window.foo = "This global variable was added by a page script";
+
+// // redefine the built-in window.confirm() function
+// window.confirm = function() {
+//   alert("The page script has also redefined 'confirm'");
+// }
